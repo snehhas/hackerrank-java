e34a8f18f30a6b22069efa37a8db69a9bf39ca72
Sneha Sunny <56714324+snehhas@users.noreply.github.com>
2022-01-17 23:10:03 +0530

    Learning Java

---
+++ hackerrank-java-list.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    List<Integer> lst = new ArrayList<>();
+    for (int i = 0; i < N; i++) {
+        lst.add(sc.nextInt());
+    }
+    int Q = sc.nextInt();
+    while (Q-- > 0) {
+        switch (sc.next()) {
+            case "Insert":
+                lst.add(sc.nextInt(), sc.nextInt());
+                break;
+            
+            case "Delete":
+                lst.remove(sc.nextInt());
+                break;
+            
+            default:
+                System.out.println("Invalid operation.");
+                System.exit(-1);
+                break;
+        }
+    }
+    sc.close();
+    while (!lst.isEmpty()) {
+        System.out.print(lst.remove(0) + " ");
+    }
+    }
+}
